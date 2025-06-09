3baea1ae184a653ebc9feebc9b319880672e1829
Entiesty <1330624184@qq.com>
2025-06-09 12:12:07 +0800

    完成了查询附近商户功能的API，并且测试成功

---
+++ src/main/java/com/example/petshopbackend/repository/mongo/StoreLocationRepository.java
@@ -0,0 +1,12 @@
+package com.example.petshopbackend.repository.mongo;
+
+import com.example.petshopbackend.entity.StoreLocation;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.geo.Point;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface StoreLocationRepository extends MongoRepository<StoreLocation, String> {
+    // 根据一个点和距离，查询附近的位置
+    GeoResults<StoreLocation> findByLocationNear(Point point, Distance distance);
+}
